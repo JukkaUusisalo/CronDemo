107ec955112651d082e85a754109aa9025300a52
JukkaUusisalo <jukka.uusisalo@gmail.com>
2018-07-18 15:30:29 +0300

    Here we go!

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "CronDemo"
+
+version := "0.1"
+
+scalaVersion := "2.12.6"
+
+// https://mvnrepository.com/artifact/org.quartz-scheduler/quartz
+libraryDependencies += "org.quartz-scheduler" % "quartz" % "2.2.1"
---
+++ src/main/scala/org/uusisalo/crondemo/Main.scala
@@ -0,0 +1,31 @@
+package org.uusisalo.crondemo
+
+import org.quartz._
+import org.quartz.impl.StdSchedulerFactory
+
+
+object Main {
+
+  def main(args: Array[String]):Unit = {
+    println("This is cron demo!")
+    initCron
+    System.in.read
+  }
+
+  def initCron() = {
+
+    lazy val cron = StdSchedulerFactory.getDefaultScheduler
+
+    val crontask = JobBuilder.newJob(classOf[CronJob]).build
+    val trigger = TriggerBuilder.newTrigger().withSchedule(CronScheduleBuilder.cronSchedule("0 0/1 * * * ?")).build
+    cron.start()
+    cron.scheduleJob(crontask,trigger)
+
+  }
+}
+
+class CronJob extends Job {
+  override def execute(context: JobExecutionContext) = {
+    println("Something happens")
+  }
+}
